a100494b13d3d3eeb67b09286874f841768ed79e
Azimatul Hanafiyah <89617795+azimatulh@users.noreply.github.com>
2021-12-13 18:43:14 -0800

    Add files via upload

---
+++ AplikasiKoperasi/src/cobafinalproject/CobaFinalProject.java
@@ -0,0 +1,32 @@
+package cobafinalproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class CobaFinalProject extends Application {
+    
+
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
